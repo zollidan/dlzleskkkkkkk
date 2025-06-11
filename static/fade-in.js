d6de3d7113f49c215d35178901a8e80695557550
Stepan Fedosov <zollidan@yandex.ru>
2025-06-11 03:26:39 +0300

    first commit

---
+++ static/fade-in.js
@@ -0,0 +1,9 @@
+const elements = document.querySelectorAll(".fade-in");
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visible");
+    }
+  });
+});
+elements.forEach((el) => observer.observe(el));
